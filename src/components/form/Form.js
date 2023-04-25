b8a9045d272bc30a399e42fb3c876cd7e9e77afd
Salman Mahmud <salman_mahmud@hotmail.com>
2023-04-24 20:50:17 -0400

    image added and spacing adjusted

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { ShoppingCart } from '@/icons/ShoppingCart'
 
 function form() {
 
@@ -20,11 +21,11 @@ function form() {
         console.log(formData.item)
     }
   return (
-    <form className='flex flex-col border-2 border-black mx-auto p-10 rounded-md items-center'  onSubmit={handleSubmit}>
-        image here
-        <input className='bg-gray-200 focus:bg-white border border-solid border-violet-600 focus:border-blue-400 rounded-md p-2 mb-2 transition duration-300 text-center' type='text' placeholder='Enter text'/>
+    <form className='flex flex-col border-2 border-black mx-auto p-10 rounded-md items-center max-w-xs'  onSubmit={handleSubmit}>
+        <ShoppingCart/>
+        <input className='bg-gray-200 focus:bg-white border border-solid border-violet-600 focus:border-blue-400 rounded-md p-2 m-2 transition duration-300 text-center' type='text' placeholder='Enter text'/>
         
-        <button className=' mx-auto px-14 bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 p-2 rounded-md hover:scale-110 hover:from-blue-400 hover:to-blue-400 text-white transition' type='submit' >Add to cart</button>
+        <button className=' mx-auto px-14 bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 py-2 rounded-md hover:scale-110 hover:from-blue-400 hover:to-blue-400 text-white transition' type='submit' >Add to cart</button>
     </form>
   )
 }
---
+++ src/icons/ShoppingCart.js
@@ -0,0 +1,17 @@
+
+export function ShoppingCart(props) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 24 24" {...props}>
+  <defs>
+    <linearGradient id="myGradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="24" y2="24">
+      <stop offset="0%" stop-color="#4B0082" /> 
+      <stop offset="50%" stop-color="#800080" /> 
+      <stop offset="100%" stop-color="#FF69B4" /> 
+    </linearGradient>
+  </defs>
+  <path fill="url(#myGradient)" d="M7 22q-.825 0-1.413-.588T5 20q0-.825.588-1.413T7 18q.825 0 1.413.588T9 20q0 .825-.588 1.413T7 22Zm10 0q-.825 0-1.413-.588T15 20q0-.825.588-1.413T17 18q.825 0 1.413.588T19 20q0 .825-.588 1.413T17 22ZM7 17q-1.125 0-1.7-.988t-.05-1.962L6.6 11.6L3 4H1.975q-.425 0-.7-.288T1 3q0-.425.288-.713T2 2h1.625q.275 0 .525.15t.375.425L5.2 4h14.75q.675 0 .925.5t-.025 1.05l-3.55 6.4q-.275.5-.725.775T15.55 13H8.1L7 15h11.025q.425 0 .7.288T19 16q0 .425-.288.713T18 17H7Z"></path>
+</svg>
+
+    
+      )
+  }

16864511330a85cf844bace8293d23864526c4ee
Salman Mahmud <salman_mahmud@hotmail.com>
2023-04-27 18:55:05 -0400

    communication established between firebase and app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 
 import Form from '@/components/form/Form'
+import firebase, { initializeApp } from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+  databaseURL: "https://playground-f55ae-default-rtdb.firebaseio.com/"
+}
+
+
+const app = initializeApp(firebaseConfig)
 
 
 export default function Home() {
+  console.log('app', app)
   return (
     <div > 
       hello
